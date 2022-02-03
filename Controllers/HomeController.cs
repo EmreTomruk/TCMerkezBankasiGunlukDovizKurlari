3eef3421ccb9cf03c83e2d9dd3102db214a981d6
Emre <emretmrk39@gmail.com>
2022-02-03 18:52:38 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using TCMBCurrencies.Models;
+
+namespace TCMBCurrencies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Currencies CurList;
+
+            WebClient webClient = new WebClient();
+
+            var json = webClient.DownloadString("http://hasanadiguzel.com.tr/api/kurgetir");
+
+            //var result = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            //List<CurrenciesModel> CurrencyList = JsonConvert.DeserializeObject<List<CurrenciesModel>>(json);
+
+            CurList = JsonConvert.DeserializeObject<Currencies>(json);
+
+
+            if (CurList == null)
+                return null;
+
+            return View(CurList);
+        }
+    }
+}
---
+++ Models/Currencies.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace TCMBCurrencies.Models
+{
+    public class Currencies
+    {
+        public Developer Developer { get; set; }
+        public List<TCMBAnlikKurBilgileri> TCMB_AnlikKurBilgileri { get; set; }
+    }
+    public class Developer
+    {
+        public string name { get; set; }
+        public string website { get; set; }
+        public string email { get; set; }
+    }
+
+    public class TCMBAnlikKurBilgileri
+    {
+        public string Isim { get; set; }
+        public string CurrencyName { get; set; }
+        public double ForexBuying { get; set; }
+        public object ForexSelling { get; set; }
+        public object BanknoteBuying { get; set; }
+        public object BanknoteSelling { get; set; }
+        public object CrossRateUSD { get; set; }
+        public object CrossRateOther { get; set; }
+    }
+}
+
